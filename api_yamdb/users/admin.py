ef870e348c0a0d4be430cf4a5adcd474e5e19179
Kostya Deumin <c-o-n-s-t-a-n-t@yandex.ru>
2024-08-14 19:12:59 +0300

    Added password log in

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 
 User = get_user_model()
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    model = User
+"""
+    fieldsets = (
+        (None, {'fields': ('username',)}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                ),
+            },
+        ),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+"""
+
+admin.site.register(User, UserAdmin)
