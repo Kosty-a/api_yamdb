bb63f4a7de790686818475b53ddf3a6f150021ff
Kostya Deumin <c-o-n-s-t-a-n-t@yandex.ru>
2024-08-15 04:09:39 +0300

    Fixed users

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -6,9 +6,14 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 User = get_user_model()
 
 
-# @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     model = User
+
+    fieldsets = BaseUserAdmin.fieldsets
+    fieldsets[1][1]['fields'] += ('bio', 'role')
+
+    list_display = ('username', 'email', 'is_staff', 'role')
+    list_editable = ('role',)
 
 
 admin.site.register(User, UserAdmin)
